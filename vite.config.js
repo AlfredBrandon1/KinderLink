1d652a0cf0dea9c273359ab744dde33c1681ff86
AlfredBrandon1 <calawa.alfredbrandon@gmail.com>
2023-04-07 11:30:07 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default {
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: 'server.js', // change this to your app's entry point
+        main: 'src/main.jsx', // change this to your app's entry point
       },
     },
   },
