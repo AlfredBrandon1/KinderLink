63128063216570d7a8334656b89f292e3ede0f7e
AlfredBrandon1 <calawa.alfredbrandon@gmail.com>
2023-04-07 19:37:46 +0800

    add dotenv

--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
-const server  = express();
+const server = express();
 const port = 8080;
 
 // Middlewares
-server.use( morgan('dev') );
-server.use( cors() );
-server.use( bodyParser.json() );
-server.use( helmet() );
+server.use(morgan('dev'));
+server.use(cors());
+server.use(bodyParser.json());
+server.use(helmet());
 
 // Enable CORS for all routes
 server.use(function(req, res, next) {
@@ -20,23 +21,23 @@ server.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
-  });
+});
 
 // Routes initialization
 const AuthRouter = require('./routes/auth');
 const TeacherRouter = require('./routes/teacher');
- 
+
 // Routes
 server.use('/api/v1/auth', AuthRouter);
 server.use('/api/v1/teacher', TeacherRouter);
 
 //mongodb atlas connection
-mongoose.connect('mongodb+srv://kinderlink:kinderlink@cluster0.t7zlntf.mongodb.net/?retryWrites=true&w=majority')
- 
-server.get('/', ( request, response ) => {
+mongoose.connect(process.env.DB_CONNECTION_STRING);
+
+server.get('/', (request, response) => {
     response.send(`Welcome to the backend server of KinderLink`);
 });
- 
+
 server.listen(port, () => {
     console.log(`Server running on port ${ port }`);
 });
