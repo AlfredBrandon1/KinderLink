c02d96f0d696fff4dc25e9fb8c8b39088f9b585f
AlfredBrandon1 <calawa.alfredbrandon@gmail.com>
2023-04-10 00:49:02 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const path = require('path');
 const server = express();
-const port = process.env.PORT || 8080;
+const port = 8080;
 
 // Middlewares
 server.use(morgan('dev'));
@@ -17,6 +17,7 @@ server.use(helmet());
 
 // Enable CORS for all routes
 server.use(function(req, res, next) {
+
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -31,26 +32,20 @@ const TeacherRouter = require('./routes/teacher');
 server.use('/api/v1/auth', AuthRouter);
 server.use('/api/v1/teacher', TeacherRouter);
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  server.use(express.static(path.join(__dirname,)));
+// Serve static assets from the "client/build" directory
+server.use(express.static(path.join(__dirname, 'client', 'build')));
 
-  // Catch all routes and return index.html
-  server.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-  });
-} else {
-  server.get('/', (request, response) => {
-    response.send(`Welcome to the backend server of KinderLink`);
-  });
-}
+// Redirect all requests to the index.html file
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 //mongodb atlas connection
 mongoose.connect("mongodb+srv://kinderlink:kinderlink@cluster0.t7zlntf.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.log(err));
 
+
 server.listen(port, () => {
-  console.log(`Server running on port ${ port }`);
+    console.log(`Server running on port ${ port }`);
 });
