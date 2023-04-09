553b0667b8d83881c11bfc059d3ebf1cb90fd986
AlfredBrandon1 <calawa.alfredbrandon@gmail.com>
2023-04-10 00:58:58 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,13 +15,15 @@ server.use(cors());
 server.use(bodyParser.json());
 server.use(helmet());
 
+// Serve static files from React app
+server.use(express.static(path.join(__dirname, 'client')));
+
 // Enable CORS for all routes
 server.use(function(req, res, next) {
-
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
 
 // Routes initialization
@@ -32,20 +34,15 @@ const TeacherRouter = require('./routes/teacher');
 server.use('/api/v1/auth', AuthRouter);
 server.use('/api/v1/teacher', TeacherRouter);
 
-// Serve static assets from the "client/build" directory
-server.use(express.static(path.join(__dirname, 'client', 'build')));
-
-// Redirect all requests to the index.html file
-server.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
-
 //mongodb atlas connection
 mongoose.connect("mongodb+srv://kinderlink:kinderlink@cluster0.t7zlntf.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.log(err));
 
+server.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
 
 server.listen(port, () => {
-    console.log(`Server running on port ${ port }`);
+  console.log(`Server running on port ${ port }`);
 });
