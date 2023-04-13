c4bb3589bc4effe681f8f192180ac59338fa5337
AlfredBrandon1 <calawa.alfredbrandon@gmail.com>
2023-04-13 08:18:10 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ server.use(express.static(path.join(__dirname, 'client')));
 server.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
